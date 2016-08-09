a78b0b79376777b235b16ef424d58860934dea9e
Sean <seon2you@hotmail.com>
2016-08-09 12:43:39 -0700

    adding addstudent and get all students

--- AngularFormMvc/Scripts/App/app.js
+++ AngularFormMvc/Scripts/App/app.js
@@ -1,1 +1,20 @@
-﻿var module = angular.module("studentApp", ["studentController", "studentResources"]);
+﻿var module = angular.module("studentApp", ["ngRoute", "studentController", "studentResources"]);
+
+module.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'Templates/home.html',
+            controller: 'getStudentsList'
+        }).
+        when('/addstudent', {
+            templateUrl: 'Templates/addstudent.html',
+            controller: 'addStudent'
+        }).
+        when('/allstudents', {
+            templateUrl: 'Templates/allstudents.html',
+            controller: 'allStudents'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+}]);
--- AngularFormMvc/Scripts/App/controller.js
+++ AngularFormMvc/Scripts/App/controller.js
@@ -4,3 +4,39 @@ studentController.controller("getStudentsList", function ($scope, $http) {
         $scope.students = data;
     });
 });
+
+studentController.controller("addStudent", function ($scope, $http, $location) {
+    $scope.student = {
+        "FirstName": "",
+        "LastName": "",
+        "Age": "",
+        "Gender": "Male"
+    };
+
+    $scope.genders = ['Male', 'Female', 'Other'];
+
+    $scope.pattern = /^\s*\d+\s*$/;
+    $scope.Add = function () {
+        $http({ method: "POST", data: $scope.student, url: "/api/student" }).
+            success(function (data, status, headers, config) {
+                $scope.students = data;
+                $scope.student = {
+                    "FirstName": "",
+                    "LastName": "",
+                    "Age": "",
+                    "Gender": ""
+                };
+            });
+        alert("Student Added Successfully..!!");
+        $location.path('/allstudents');
+    }
+});
+
+studentController.controller("allStudents", function ($scope, $http) {
+    $scope.getAll = function () {
+        $http({ method: "GET", url: "/api/student" }).
+            success(function (data, status, headers, config) {
+                $scope.students = data;
+            });
+    }
+});
