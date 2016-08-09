b8b3aa86e217f4ada814b71bb3d6920b97e5a9e5
Sean <seon2you@hotmail.com>
2016-08-09 10:24:44 -0700

    engaged angular

---
+++ AngularFormMvc/Scripts/App/controller.js
@@ -0,0 +1,6 @@
+﻿var studentController = angular.module("studentController", []);
+studentController.controller("getStudentsList", function ($scope, $http) {
+    $http.get("/api/student").success(function (data) {
+        $scope.students = data;
+    });
+});
